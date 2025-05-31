579c603a1ee5dfb240f2d84566521af0f45c9488
mintyshirt <mintyshirt1@gmail.com>
2025-05-31 11:08:03 +0200

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,7 @@ import App from './App';
 import { Web3Provider } from './Web3Context';
 import { ThemeProvider } from './ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
-
 const root = document.getElementById('root');
-
 if (root) {
   createRoot(root).render(
     <React.StrictMode>

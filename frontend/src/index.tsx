ac55921d09bd1a887b1033cacf3b078fd3a1abe0
mintyshirt <mintyshirt1@gmail.com>
2025-05-31 10:29:34 +0200

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Web3Provider } from './contexts/Web3Context';
+import { Web3Provider } from './Web3Context';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
 

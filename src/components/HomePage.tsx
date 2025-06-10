04d7c052d2e3d87f78142f8c950133033b99f57e
mintyshirt <mintyshirt1@gmail.com>
2025-06-10 12:19:38 +0200

    Adjust navbar spacing

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -19,7 +19,7 @@ function Navbar() {
 
   return (
     <nav className="sticky top-0 bg-[#2C1D59] text-white shadow z-10">
-      <div className="max-w-7xl mx-auto p-4 space-y-2">
+      <div className="max-w-7xl mx-auto p-4 space-y-4">
         <div className="flex items-center justify-end space-x-4">
           <input
             type="text"
